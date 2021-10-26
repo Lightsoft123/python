9861e9827c8f468e47d2e795cf518cccad20bc17
Lightsoft123 <stevenfebrianto871@gmail.com>
2021-10-26 15:43:37 +0700

    learning about list with python

--- learn_list.py
+++ learn_list.py
@@ -3,7 +3,7 @@ print(type(test_list)) #prove for this variable is list type
 for i in range(len(test_list)): #looping from index 0 to length of test_list-1 (it means, from 0 to 2)
     print(test_list[i]) #print the test_list of i-th index
 test_list = list(("50", "abc", "cde")) #Create a list same like before, but it used list built-in function (note : use 2 round-bracket)
-print(test_llist) #print the proof of same declare list like before
+print(test_list) #print the proof of same declare list like before
 test_list = list(("1", "2", "3", "4", "5", "6")) #re-declare test_list variable
 print(test_list[1:len(test_list)]) #it works like using looping from index i-th to length of test_list-1
 print(test_list[1:5]) #other example for using "var[index:index]" (works like looping from 1st index to 5-1 = 4th index)
@@ -67,7 +67,7 @@ print(test_list)
 #output : ['1', '2.11', '2.12', '2.2', '3', '5', '6', '7', '8', '9', '10']
 
 del test_list #delete test_list variable
-print(test_list)
+#print(test_list)
 #output : NameError: name 'test_list' is not defined
 #output will cause an error because "test_list" variable has been deleted and it hasn't declared yet.
 
